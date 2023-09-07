f46dff981e224ce60400eb7c04141b209e54641d
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-09-07 23:01:50 +0300

    Update REX_servo_test.ino

--- Servo Calibration/REX_servo_test/REX_servo_test.ino
+++ Servo Calibration/REX_servo_test/REX_servo_test.ino
@@ -4,7 +4,7 @@ Servo myservo1;
 Servo myservo2;
 Servo myservo3;
 Servo myservo4;
-int pos = 0;
+
 int servoPin1 = 2;
 int servoPin2 = 26;
 int servoPin3 = 18;
@@ -18,41 +18,8 @@ void setup() {
 }
 
 void loop() {
-  myservo1.write(0);
-  myservo2.write(0);
-  myservo3.write(0);
-  myservo4.write(0);
-  delay(2000);
-  for (pos = 0; pos <= 90; pos += 1) {
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-    myservo4.write(pos);
-    delay(15);
-  }
-  delay(2000);
-  for (pos = 90; pos <= 180; pos += 1) {
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-    myservo4.write(pos);
-    delay(15);
-  }
-  delay(2000);
-  for (pos = 180; pos >= 90; pos -= 1) {
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-    myservo4.write(pos);
-    delay(15);
-  }
-  delay(2000);
-  for (pos = 90; pos >= 0; pos -= 1) {
-    myservo1.write(pos);
-    myservo2.write(pos);
-    myservo3.write(pos);
-    myservo4.write(pos);
-    delay(15);
-  }
-  delay(2000);
+  myservo1.write(90);
+  myservo2.write(90);
+  myservo3.write(90);
+  myservo4.write(90);
 }
