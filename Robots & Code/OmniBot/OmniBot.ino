7acd7ecccc943f3251ab217bfb55fae368ad35ab
Mehmet Ali <78815495+mehmetalidag97@users.noreply.github.com>
2023-10-09 16:23:19 +0300

    Update OmniBot.ino

--- Robots & Code/OmniBot/OmniBot.ino
+++ Robots & Code/OmniBot/OmniBot.ino
@@ -3,40 +3,70 @@
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
-
 #include <DabbleESP32.h>
 #include <Arduino.h>
-#include <analogWrite.h>
 #include <ESP32Servo.h>
 
+enum MOTOR_TYPE {
+  DC_MOTOR,
+  SERVO_MOTOR
+};
 
-//define pins of motors
-//DC Motor_1
-#define MotorA1 23
-#define MotorA2 15
+enum MOTOR_TYPE motorType = DC_MOTOR;
 
-//DC Motor_2
-#define MotorB1 33
-#define MotorB2 32
+#define speed 12
+//define Motor Pins
+#define MotorA1 15  // Forward
+#define MotorA2 23  // Backward
 
-//DC Motor_3
-#define MotorC1 16
-#define MotorC2 17
+#define MotorB1 32  // Forward
+#define MotorB2 33  // Backward
 
-//DC Motor_4
-#define MotorD1 14
-#define MotorD2 27
-#define horn 2
+#define MotorC1 5  // Forward
+#define MotorC2 4  // Backward
 
-#define low_s 100
-#define mid_s 150
-#define high_s 250
+#define MotorD1 27  // Forward
+#define MotorD2 14  // Backward
 
-int i = 0 ;
+int position1 = 90;
+int position2 = 90;
+int position3 = 90;
+int position4 = 90;
+
+//define buzzer pin named "horn"
+int horn = 25;
+
+//define pins of servo motors
+Servo Servo1;  // Forward-Bakcward
+Servo Servo2;  // Right-Legt
+Servo Servo3;  // Up-Down
+Servo Servo4;  // Open-Close
+
 void setup() {
-  //active pins which is defined
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
 
+  Servo1.setPeriodHertz(50);
+  Servo2.setPeriodHertz(50);
+  Servo3.setPeriodHertz(50);
+  Servo4.setPeriodHertz(50);
+  //defined active pins
   pinMode(horn, OUTPUT);
+
+  Servo1.attach(2, 600, 2500);
+  Servo2.attach(26, 600, 2500);
+  Servo3.attach(18, 600, 2500);
+  Servo4.attach(19, 600, 2500);
+
+  //first positions of servo motors
+  Servo1.write(position1);
+  Servo2.write(position2);
+  Servo3.write(position3);
+  Servo4.write(position4);
+
+  pinMode(speed, OUTPUT);
 
   pinMode(MotorA1, OUTPUT);
   pinMode(MotorA2, OUTPUT);
@@ -50,469 +80,254 @@ void setup() {
   pinMode(MotorD1, OUTPUT);
   pinMode(MotorD2, OUTPUT);
 
-  Serial.begin(115200); // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin("REX_OMNI"); //set bluetooth name of your device
+  Serial.begin(115200);       // make sure your Serial Monitor is also set at this baud rate.
+  Dabble.begin("REX_ROBOT");  //set bluetooth name of your device
 }
 
-
 void loop() {
-  delay(20);
-  Dabble.processInput();
-  Serial.println('\t');
+  //Print of servo motor position on Serial Port
+  /*Serial.println(position1);
+    Serial.println(position2);
+    Serial.println(position3);
+    Serial.println(position4);*/
 
+  //This function is used to keep information coming from the mobile device up to date.
+  Dabble.processInput();
+  stop();
+
+  if (GamePad.isSelectPressed()) {
+    motorType = SERVO_MOTOR;
+    digitalWrite(horn, HIGH);
+    delay(100);
+    digitalWrite(horn, LOW);
+    delay(1);
+
+    int position1 = 90;
+    int position2 = 90;
+    int position3 = 90;
+    int position4 = 90;
+    
+  }
+  if (GamePad.isStartPressed()) {
+    motorType = DC_MOTOR;
+    digitalWrite(horn, HIGH);
+    delay(100);
+    digitalWrite(horn, LOW);
+    delay(1);
+    digitalWrite(horn, HIGH);
+    delay(100);
+    digitalWrite(horn, LOW);
+    delay(1);
+  }
+
+  switch (motorType) {
+    case DC_MOTOR:
+      //Serial.println("DC Turn On");
+      dc_motor();
+      break;
+    case SERVO_MOTOR:
+      //erial.println("Servo Turn On");
+      servo_motor();
+
+      break;
+  }
+
+  //Serial.println('\t');
   int a = GamePad.getAngle();
-  Serial.print("Angle: ");
-  Serial.print(a);
-  Serial.print('\t');
+  /*Serial.print("Angle: ");
+    Serial.print(a);
+    Serial.print('\t');*/
 
   int b = GamePad.getRadius();
-  Serial.print("Radius: ");
-  Serial.print(b);
-  Serial.print('\t');
+  /*Serial.print("Radius: ");
+    Serial.print(b);
+    Serial.print('\t');*/
 
-  if (GamePad.isCrossPressed())
-  {
-    Serial.print("DC Çarpı");
-    tone(horn, 330);
-    delay(100);
-    noTone(2);
+  float c = GamePad.getXaxisData();
+  /*Serial.print("x_axis: ");
+    Serial.print(c);
+    Serial.print('\t');*/
+
+  float d = GamePad.getYaxisData();
+  /*Serial.print("y_axis: ");
+    Serial.println(d);
+    Serial.println();*/
+}
+
+void servo_motor() {
+
+  if (GamePad.isUpPressed()) {
+    if (position2 > 0) {
+      position2 = position2 - 1;
+    }
   }
-  if (GamePad.isSquarePressed())
-  {
+  if (GamePad.isDownPressed()) {
+    if (position2 < 140) {
+      position2 = position2 + 1;
+    }
   }
-  /////////////////////////////YUKARI////////////////////////////////////
-
-  if (a > 60 && a < 120 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, low_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, low_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, low_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, low_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isRightPressed()) {
+    if (position1 < 140) {
+      position1 = position1 + 1;
+    }
   }
-
-  else if (a > 60 && a < 120 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, mid_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, mid_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, mid_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, mid_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isLeftPressed()) {
+    if (position1 > 40) {
+      position1 = position1 - 1;
+    }
+  }
+  if (GamePad.isSquarePressed()) {
+    if (position4 < 160) {
+      position4 = position4 + 1;
+    }
   }
 
-  else if (a > 60 && a < 120 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, high_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, high_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, high_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, high_s);
-    analogWrite(MotorD2, 0);
-  }
-  /////////////////////////////AŞAĞI////////////////////////////////////
-
-  else if (a > 240 && a < 300 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, low_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, low_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, low_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, low_s);
+  if (GamePad.isCirclePressed()) {
+    if (position4 > 90) {
+      position4 = position4 - 1;
+    }
   }
 
-  else if (a > 240 && a < 300 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, mid_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, mid_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, mid_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, mid_s);
+  if (GamePad.isCrossPressed()) {
+    if (position3 > 30) {
+      position3 = position3 - 1;
+    }
   }
 
-  else if (a > 240 && a < 300 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, high_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, high_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, high_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, high_s);
+  if (GamePad.isTrianglePressed()) {
+    if (position3 < 150) {
+      position3 = position3 + 1;
+    }
   }
 
-  /////////////////////////////SAĞ////////////////////////////////////
-  //////////////////////////////0-30////////////////
-  else if (a >= 0 && a < 30 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, low_s);
-    analogWrite(MotorA2, 0);
+  delay(10);
 
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, low_s);
+  Servo1.write(position1);
+  Servo2.write(position2);
+  Servo3.write(position3);
+  Servo4.write(position4);
+}
 
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, low_s);
-
-    analogWrite(MotorD1, low_s);
-    analogWrite(MotorD2, 0);
+void dc_motor() {
+  if (GamePad.isUpPressed()) {
+    forward();
   }
 
-  else if (a >= 0 && a < 30 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, mid_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, mid_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, mid_s);
-
-    analogWrite(MotorD1, mid_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isDownPressed()) {
+    backward();
   }
 
-  else if (a >= 0 && a < 30 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, high_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, high_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, high_s);
-
-    analogWrite(MotorD1, high_s);
-    analogWrite(MotorD2, 0);
-  }
-  //////////////////////////330-360///////////////////////////
-  else if (a > 330 && a < 360 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, low_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, low_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, low_s);
-
-    analogWrite(MotorD1, low_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isLeftPressed()) {
+    left();
   }
 
-  else if (a > 330 && a < 360 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, mid_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, mid_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, mid_s);
-
-    analogWrite(MotorD1, mid_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isRightPressed()) {
+    right();
+  }
+  if (GamePad.isSquarePressed()) {
   }
 
-  else if (a > 330 && a < 360 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, high_s);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, high_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, high_s);
-
-    analogWrite(MotorD1, high_s);
-    analogWrite(MotorD2, 0);
+  if (GamePad.isCirclePressed()) {
+    for (int i = 0; i < 3; i++) {
+      forward();
+      //tone(horn, 330);
+      delay(300);
+      left();
+      //tone(horn, 430);
+      delay(300);
+      right();
+      //tone(horn, 530);
+      delay(300);
+      // tone(horn, 630);
+      left();
+      delay(300);
+      //noTone(horn);
+    }
   }
 
-  /////////////////////////////SOL////////////////////////////////////
-
-  else if (a > 150 && a < 210 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, low_s);
-
-    analogWrite(MotorB1, low_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, low_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, low_s);
+  if (GamePad.isCrossPressed()) {
+    //Serial.print("DC Cross");
+    digitalWrite(horn, HIGH);
+    delay(200);
+    digitalWrite(horn, LOW);
+    delay(1);
   }
 
-  else if (a > 150 && a < 210 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, mid_s);
-
-    analogWrite(MotorB1, mid_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, mid_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, mid_s);
+  if (GamePad.isTrianglePressed()) {
+    //Serial.print("DC Triangle");
+    Servo1.write(90);
+    Servo2.write(90);
+    Servo3.write(90);
+    Servo4.write(90);
   }
+}
 
-  else if (a > 150 && a < 210 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, high_s);
+void forward() {
+  digitalWrite(MotorA1, HIGH);
+  digitalWrite(MotorA2, LOW);
 
-    analogWrite(MotorB1, high_s);
-    analogWrite(MotorB2, 0);
+  digitalWrite(MotorB1, HIGH);
+  digitalWrite(MotorB2, LOW);
 
-    analogWrite(MotorC1, high_s);
-    analogWrite(MotorC2, 0);
+  digitalWrite(MotorC1, HIGH);
+  digitalWrite(MotorC2, LOW);
 
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, high_s);
-  }
+  digitalWrite(MotorD1, HIGH);
+  digitalWrite(MotorD2, LOW);
+}
 
-  //////////////////////////////////ARA YÖNLER///////////////////////////////////////////
-  /////////////////////////////SAĞ-YUKARI////////////////////////////////////
+void right() {
+  digitalWrite(MotorA1, HIGH);
+  digitalWrite(MotorA2, LOW);
 
-  else if (a >= 30 && a <= 60 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, low_s);
-    analogWrite(MotorA2, 0);
+  digitalWrite(MotorB1, HIGH);
+  digitalWrite(MotorB2, LOW);
 
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
+  digitalWrite(MotorC1, LOW);
+  digitalWrite(MotorC2, HIGH);
 
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
+  digitalWrite(MotorD1, LOW);
+  digitalWrite(MotorD2, HIGH);
+}
 
-    analogWrite(MotorD1, low_s);
-    analogWrite(MotorD2, 0);
-  }
+void left() {
+  digitalWrite(MotorA1, LOW);
+  digitalWrite(MotorA2, HIGH);
 
-  else if (a >= 30 && a <= 60 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, mid_s);
-    analogWrite(MotorA2, 0);
+  digitalWrite(MotorB1, LOW);
+  digitalWrite(MotorB2, HIGH);
 
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
+  digitalWrite(MotorC1, HIGH);
+  digitalWrite(MotorC2, LOW);
 
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
+  digitalWrite(MotorD1, HIGH);
+  digitalWrite(MotorD2, LOW);
+}
 
-    analogWrite(MotorD1, mid_s);
-    analogWrite(MotorD2, 0);
-  }
+void stop() {
+  digitalWrite(MotorA1, LOW);
+  digitalWrite(MotorA2, LOW);
 
-  else if (a >= 30 && a <= 60 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, high_s);
-    analogWrite(MotorA2, 0);
+  digitalWrite(MotorB1, LOW);
+  digitalWrite(MotorB2, LOW);
 
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
+  digitalWrite(MotorC1, LOW);
+  digitalWrite(MotorC2, LOW);
 
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
+  digitalWrite(MotorD1, LOW);
+  digitalWrite(MotorD2, LOW);
+}
 
-    analogWrite(MotorD1, high_s);
-    analogWrite(MotorD2, 0);
-  }
+void backward() {
+  digitalWrite(MotorA1, LOW);
+  digitalWrite(MotorA2, HIGH);
 
-  /////////////////////////////SOL-YUKARI////////////////////////////////////
+  digitalWrite(MotorB1, LOW);
+  digitalWrite(MotorB2, HIGH);
 
-  else if (a >= 120 && a <= 150 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
+  digitalWrite(MotorC1, LOW);
+  digitalWrite(MotorC2, HIGH);
 
-    analogWrite(MotorB1, low_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, low_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-
-  else if (a >= 120 && a <= 150 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, mid_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, mid_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-
-  else if (a >= 120 && a <= 150 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, high_s);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, high_s);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-  /////////////////////////////SOL-AŞAĞI////////////////////////////////////
-
-  else if (a >= 210 && a <= 240 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, low_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, low_s);
-  }
-
-  else if (a >= 210 && a <= 240 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, mid_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, mid_s);
-  }
-
-  else if (a >= 210 && a <= 240 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, high_s);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, 0);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, 0);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, high_s);
-  }
-
-
-  /////////////////////////////SAĞ-AŞAĞI////////////////////////////////////
-
-  else if (a >= 300 && a <= 330 && b >= 1 && b <= 3)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, low_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, low_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-
-  else if (a >= 300 && a <= 330 && b > 3 && b <= 5)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, mid_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, mid_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-
-  else if (a >= 300 && a <= 330 && b > 5 && b <= 7)
-  {
-    analogWrite(MotorA1, 0);
-    analogWrite(MotorA2, 0);
-
-    analogWrite(MotorB1, 0);
-    analogWrite(MotorB2, high_s);
-
-    analogWrite(MotorC1, 0);
-    analogWrite(MotorC2, high_s);
-
-    analogWrite(MotorD1, 0);
-    analogWrite(MotorD2, 0);
-  }
-  /////////////////////////////DUR////////////////////////////////////
-  else
-  {
-    analogWrite(MotorA1, LOW);
-    analogWrite(MotorA2, LOW);
-
-    analogWrite(MotorB1, LOW);
-    analogWrite(MotorB2, LOW);
-
-    analogWrite(MotorC1, LOW);
-    analogWrite(MotorC2, LOW);
-
-    analogWrite(MotorD1, LOW);
-    analogWrite(MotorD2, LOW);
-  }
+  digitalWrite(MotorD1, LOW);
+  digitalWrite(MotorD2, HIGH);
 }
