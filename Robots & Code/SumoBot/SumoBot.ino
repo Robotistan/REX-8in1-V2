297f921773bee66e8d7fd7d5bb2cfe5cfa774e10
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2023-10-13 16:25:47 +0300

    Update SumoBot.ino

--- Robots & Code/SumoBot/SumoBot.ino
+++ Robots & Code/SumoBot/SumoBot.ino
@@ -1,10 +1,8 @@
-#define SensorSol 34 // IR pinleri
-#define SensorSag 35 // IR pinleri
-int trigPin = 4;    // Trigger
-int echoPin = 5;    // Echo
+#define SensorSol 34 // IR pin
+#define SensorSag 35 // IR pin
+int trigPin = 17;    // Trigger
+int echoPin = 16;    // Echo
 long duration, cm;
-
-// Motor pinlerini tanımlıyoruz.
 
 #define MotorA1 15
 #define MotorA2 23
@@ -15,17 +13,86 @@ long duration, cm;
 #define MotorC1 5
 #define MotorC2 4
 
-#define MotorD1 14
-#define MotorD2 27
+#define MotorD1 27
+#define MotorD2 14
 
 #define mid 115
 #define slow 0
-#define THRESHOLD   600
+#define THRESHOLD   3600
+
+void forward() {
+  analogWrite(MotorA1, mid);
+  analogWrite(MotorA2, LOW);
+
+  analogWrite(MotorB1, mid);
+  analogWrite(MotorB2, LOW);
+
+  analogWrite(MotorC1, mid);
+  analogWrite(MotorC2, LOW);
+
+  analogWrite(MotorD1, mid);
+  analogWrite(MotorD2, LOW);
+}
+
+void right() {
+  analogWrite(MotorA1, mid);
+  analogWrite(MotorA2, LOW);
+
+  analogWrite(MotorB1, mid);
+  analogWrite(MotorB2, LOW);
+
+  analogWrite(MotorC1, LOW);
+  analogWrite(MotorC2, mid);
+
+  analogWrite(MotorD1, LOW);
+  analogWrite(MotorD2, mid);
+}
+
+void left() {
+  analogWrite(MotorA1, LOW);
+  analogWrite(MotorA2, mid);
+
+  analogWrite(MotorB1, LOW);
+  analogWrite(MotorB2, mid);
+
+  analogWrite(MotorC1, mid);
+  analogWrite(MotorC2, LOW);
+
+  analogWrite(MotorD1, mid);
+  analogWrite(MotorD2, LOW);
+}
+
+void stop() {
+  analogWrite(MotorA1, LOW);
+  analogWrite(MotorA2, LOW);
+
+  analogWrite(MotorB1, LOW);
+  analogWrite(MotorB2, LOW);
+
+  analogWrite(MotorC1, LOW);
+  analogWrite(MotorC2, LOW);
+
+  analogWrite(MotorD1, LOW);
+  analogWrite(MotorD2, LOW);
+}
+
+void backward() {
+  analogWrite(MotorA1, LOW);
+  analogWrite(MotorA2, mid);
+
+  analogWrite(MotorB1, LOW);
+  analogWrite(MotorB2, mid);
+
+  analogWrite(MotorC1, LOW);
+  analogWrite(MotorC2, mid);
+
+  analogWrite(MotorD1, LOW);
+  analogWrite(MotorD2, mid);
+}
 
 void setup() {
-
   Serial.begin(115200);
-  // Giriş ve çıkışları tanımladığımız pinlerde belirtiyoruz.
+
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
@@ -46,8 +113,6 @@ void setup() {
 }
 
 void loop() {
-  
-
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
@@ -61,95 +126,28 @@ void loop() {
   Serial.println();
   delay(100);
   if  (cm < 15) {
-    ileri();
+    forward();
   }
 
   int leftSensor = analogRead(SensorSol);
   int rightSensor = analogRead(SensorSag);
+  /*
+  Serial.print("leftSensor ");
+  Serial.print(leftSensor);
+  Serial.print("rightSensor ");
+  Serial.println(rightSensor);
+ */ 
 
-  if (leftSensor >= THRESHOLD && rightSensor >= THRESHOLD) {
-    ileri();
-  }
-  else {
-    geri();
+  if ((leftSensor >= THRESHOLD && rightSensor >= THRESHOLD))  {
+    backward();
     delay(1000);
-    sol();
+    left();
     delay(400);
   }
-
-void ileri() {
-
-  analogWrite(MotorA1, mid);
-  analogWrite(MotorA2, LOW);
-
-  analogWrite(MotorB1, mid);
-  analogWrite(MotorB2, LOW);
-
-  analogWrite(MotorC1, mid);
-  analogWrite(MotorC2, LOW);
-
-  analogWrite(MotorD1, mid);
-  analogWrite(MotorD2, LOW);
-}
-
-void sag() {
-  analogWrite(MotorA1, mid);
-  analogWrite(MotorA2, LOW);
-
-  analogWrite(MotorB1, mid);
-  analogWrite(MotorB2, LOW);
-
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, mid);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, mid);
-}
-
-void sol() {
-
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, mid);
-
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, mid);
-
-  analogWrite(MotorC1, mid);
-  analogWrite(MotorC2, LOW);
-
-  analogWrite(MotorD1, mid);
-  analogWrite(MotorD2, LOW);
-}
-
-void dur() {
-
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, LOW);
-
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, LOW);
-
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, LOW);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, LOW);
-}
-
-void geri() {
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, mid);
-
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, mid);
-
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, mid);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, mid);
-}
-
-void HC_ileri() {
-  
+  else if((cm < 15) && (leftSensor <= THRESHOLD && rightSensor <= THRESHOLD)){
+    forward();
   }
+  else {  
+    forward();
+  }
+}
