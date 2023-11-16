6f71e339c38cdca43507f476688fb35eb485b45d
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2023-11-16 14:53:05 +0300

    Update SonicBot.ino

--- Robots & Code/SonicBot/SonicBot.ino
+++ Robots & Code/SonicBot/SonicBot.ino
@@ -1,11 +1,16 @@
-int trigPin = 4;    // Trigger
-int echoPin = 5;    // Echo
+//"""REX 8in1 Sonic Bot"""
+//Check the web site for Robots https://rex-rdt.readthedocs.io/en/latest/
+
+int trigPin = 17;    // Trigger
+int echoPin = 16;    // Echo
 long duration, cm;
 
-#define slow 120
+//define speed of motors
+#define SLOW 120
 #define MID 140
 #define FAST 110
 
+//define pins of motors
 #define MotorA1 15
 #define MotorA2 23
 
@@ -18,14 +23,26 @@ long duration, cm;
 #define MotorD1 14
 #define MotorD2 27
 
-#define horn 2
+//define buzzer pin named "horn"
+#define horn 25
+
+//setting PWM properties
+const int freq = 50;
+const int PWMchannel_1 = 4;
+const int PWMchannel_2 = 5;
+const int PWMchannel_3 = 6;
+const int PWMchannel_4 = 7;
+const int PWMchannel_5 = 8;
+const int PWMchannel_6 = 9;
+const int PWMchannel_7 = 10;
+const int PWMchannel_8 = 11;
+
+const int resolution = 8;
 
 int turns = 0 ;
-void setup() {
-  //Serial Port begin
-  Serial.begin (9600);
 
-  //Define inputs and outputs
+void setup() {
+  Serial.begin (115200);   //make sure your Serial Monitor is also set at this baud rate.
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -43,6 +60,30 @@ void setup() {
 
   pinMode(MotorD1, OUTPUT);
   pinMode(MotorD2, OUTPUT);
+
+  ledcSetup(PWMchannel_1, freq, resolution);
+  ledcAttachPin(MotorA1, PWMchannel_1);
+
+  ledcSetup(PWMchannel_2, freq, resolution);
+  ledcAttachPin(MotorA2, PWMchannel_2);
+
+  ledcSetup(PWMchannel_3, freq, resolution);
+  ledcAttachPin(MotorB1, PWMchannel_3);
+
+  ledcSetup(PWMchannel_4, freq, resolution);
+  ledcAttachPin(MotorB2, PWMchannel_4);
+
+  ledcSetup(PWMchannel_5, freq, resolution);
+  ledcAttachPin(MotorC1, PWMchannel_5);
+
+  ledcSetup(PWMchannel_6, freq, resolution);
+  ledcAttachPin(MotorC2, PWMchannel_6);
+
+  ledcSetup(PWMchannel_7, freq, resolution);
+  ledcAttachPin(MotorD1, PWMchannel_7);
+
+  ledcSetup(PWMchannel_8, freq, resolution);
+  ledcAttachPin(MotorD2, PWMchannel_8);
   delay(1500);
 }
 
@@ -51,104 +92,98 @@ void loop() {
   Serial.println(turns);
   distance();
   if (cm < 8) {
-    geri();
-    horn2();
-    yerindesol();
-    dur();
+    backward();
+    digitalWrite(horn, HIGH);
+    delay(100);
+    digitalWrite(horn, LOW);
+    delay(1);
+    left();
+    stop();
     delay(100);
     turns++ ;
   }
   else
   {
-    ileri();
+    forward();
   }
-  if ( turns > 2)
+  
+  if (turns > 2)
   {
-    dur();
+    stop();
   }
 }
 
-void ileri() { // ileri yönde hareketi için fonksiyon tanımlıyoruz.
+void forward() { 
+  ledcWrite(PWMchannel_1, MID); //MotorA1
+  ledcWrite(PWMchannel_2, LOW); //MotorA2
 
-  analogWrite(MotorA1, MID);
-  analogWrite(MotorA2, LOW);
+  ledcWrite(PWMchannel_3, MID); //MotorB1
+  ledcWrite(PWMchannel_4, LOW); //MotorB2
 
-  analogWrite(MotorB1, MID);
-  analogWrite(MotorB2, LOW);
+  ledcWrite(PWMchannel_5, MID); //MotorC1
+  ledcWrite(PWMchannel_6, LOW); //MotorC2
 
-  analogWrite(MotorC1, MID);
-  analogWrite(MotorC2, LOW);
-
-  analogWrite(MotorD1, MID);
-  analogWrite(MotorD2, LOW);
+  ledcWrite(PWMchannel_7, MID); //MotorD1
+  ledcWrite(PWMchannel_8, LOW); //MotorD2
 }
 
-void yerindesag() { // sağa dönme hareketi için fonksiyon tanımlıyoruz.
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, FAST);
+void right() { 
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, FAST); //MotorA2
 
-  analogWrite(MotorB1, FAST);
-  analogWrite(MotorB2, LOW);
+  ledcWrite(PWMchannel_3, FAST); //MotorB1
+  ledcWrite(PWMchannel_4, LOW); //MotorB2
 
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, FAST);
+  ledcWrite(PWMchannel_5, LOW); //MotorC1
+  ledcWrite(PWMchannel_6, FAST); //MotorC2
 
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, FAST);
+  ledcWrite(PWMchannel_7, LOW); //MotorD1
+  ledcWrite(PWMchannel_8, FAST); //MotorD2
   delay(1000);
 }
 
-void yerindesol() { // sola dönme hareketi için fonksiyon tanımlıyoruz.
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, MID);
+void left() { 
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, MID); //MotorA2
 
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, MID);
+  ledcWrite(PWMchannel_3, LOW); //MotorB1
+  ledcWrite(PWMchannel_4, MID); //MotorB2
 
-  analogWrite(MotorC1, MID);
-  analogWrite(MotorC2, LOW);
+  ledcWrite(PWMchannel_5, MID); //MotorC1
+  ledcWrite(PWMchannel_6, LOW); //MotorC2
 
-  analogWrite(MotorD1, MID);
-  analogWrite(MotorD2, LOW);
+  ledcWrite(PWMchannel_7, MID); //MotorD1
+  ledcWrite(PWMchannel_8, LOW); //MotorD2
   delay(850);
 }
 
-void dur() { // durma hareketi için fonksiyon tanımlıyoruz.
+void stop() { 
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, LOW); //MotorA2
 
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, LOW);
+  ledcWrite(PWMchannel_3, LOW); //MotorB1
+  ledcWrite(PWMchannel_4, LOW); //MotorB2
 
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, LOW);
+  ledcWrite(PWMchannel_5, LOW); //MotorC1
+  ledcWrite(PWMchannel_6, LOW); //MotorC2
 
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, LOW);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, LOW);
-
+  ledcWrite(PWMchannel_7, LOW); //MotorD1
+  ledcWrite(PWMchannel_8, LOW); //MotorD2
 }
 
-void geri() { // geri hareketi için fonksiyon tanımlıyoruz.
+void backward() { 
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, SLOW); //MotorA2
 
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, slow);
+  ledcWrite(PWMchannel_3, LOW); //MotorB1
+  ledcWrite(PWMchannel_4, SLOW); //MotorB2
 
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, slow);
+  ledcWrite(PWMchannel_5, LOW); //MotorC1
+  ledcWrite(PWMchannel_6, SLOW); //MotorC2
 
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, slow);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, slow);
+  ledcWrite(PWMchannel_7, LOW); //MotorD1
+  ledcWrite(PWMchannel_8, SLOW); //MotorD2
   delay(200);
-}
-
-void horn2() {
-  tone(horn, 330);
-  delay(50);
-  noTone(2);
 }
 
 void distance() {
@@ -162,9 +197,10 @@ void distance() {
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
-
   cm = (duration / 2) / 29.1;
-  /* Serial.print(cm);
-    Serial.print("cm");
-    Serial.println();*/
+  /* 
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  */
 }
