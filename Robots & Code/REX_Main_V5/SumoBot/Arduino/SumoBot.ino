994b90b600c1b8cc7f5724ac72c4dcd3345af599
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-06-26 15:14:44 +0300

    Update SumoBot.ino

--- Robots & Code/REX_Main_V5/SumoBot/Arduino/SumoBot.ino
+++ Robots & Code/REX_Main_V5/SumoBot/Arduino/SumoBot.ino
@@ -1,8 +1,11 @@
+//"""REX 8in1 Sumo Bot"""
+//Check the web site for Robots https://rex-rdt.readthedocs.io/en/latest/
+
 #define SensorLEFT 34   // IR pin
 #define SensorRIGHT 35  // IR pin
-int trigPin = 17;       // Trigger
-int echoPin = 16;       // Echo
-long duration, cm;
+
+//define pins of motors
+#define MotorPWM 13	//Pwm
 
 #define MotorA1 15
 #define MotorA2 23
@@ -16,102 +19,40 @@ long duration, cm;
 #define MotorD1 27
 #define MotorD2 14
 
+//define speed of motors
+#define FAST 255
+#define MID 130
+#define SLOW 100
 
-#define fast 255
-#define mid 150
-#define slow 100
 #define THRESHOLD 3600
 
-void forward() {
-  analogWrite(MotorA1, mid);
-  analogWrite(MotorA2, 0);
+int trigPin = 17;   // Trigger
+int echoPin = 16;   // Echo
+long duration, cm;
 
-  analogWrite(MotorB1, mid);
-  analogWrite(MotorB2, 0);
+//setting PWM properties
+const int freq = 50;
+const int PWMchannel_1 = 4;
+const int PWMchannel_2 = 5;
+const int PWMchannel_3 = 6;
+const int PWMchannel_4 = 7;
+const int PWMchannel_5 = 8;
+const int PWMchannel_6 = 9;
+const int PWMchannel_7 = 10;
+const int PWMchannel_8 = 11;
 
-  analogWrite(MotorC1, mid);
-  analogWrite(MotorC2, 0);
-
-  analogWrite(MotorD1, mid);
-  analogWrite(MotorD2, 0);
-}
-void slowforward() {
-  analogWrite(MotorA1, slow);
-  analogWrite(MotorA2, 0);
-
-  analogWrite(MotorB1, slow);
-  analogWrite(MotorB2, 0);
-
-  analogWrite(MotorC1, slow);
-  analogWrite(MotorC2, 0);
-
-  analogWrite(MotorD1, slow);
-  analogWrite(MotorD2, 0);
-}
-void right() {
-  analogWrite(MotorA1, mid);
-  analogWrite(MotorA2, 0);
-
-  analogWrite(MotorB1, mid);
-  analogWrite(MotorB2, 0);
-
-  analogWrite(MotorC1, 0);
-  analogWrite(MotorC2, mid);
-
-  analogWrite(MotorD1, 0);
-  analogWrite(MotorD2, mid);
-}
-
-void left() {
-  analogWrite(MotorA1, 0);
-  analogWrite(MotorA2, mid);
-
-  analogWrite(MotorB1, 0);
-  analogWrite(MotorB2, mid);
-
-  analogWrite(MotorC1, mid);
-  analogWrite(MotorC2, 0);
-
-  analogWrite(MotorD1, mid);
-  analogWrite(MotorD2, 0);
-}
-
-void stop() {
-  analogWrite(MotorA1, 0);
-  analogWrite(MotorA2, 0);
-
-  analogWrite(MotorB1, 0);
-  analogWrite(MotorB2, 0);
-
-  analogWrite(MotorC1, 0);
-  analogWrite(MotorC2, 0);
-
-  analogWrite(MotorD1, 0);
-  analogWrite(MotorD2, 0);
-}
-
-void backward() {
-  analogWrite(MotorA1, LOW);
-  analogWrite(MotorA2, mid);
-
-  analogWrite(MotorB1, LOW);
-  analogWrite(MotorB2, mid);
-
-  analogWrite(MotorC1, LOW);
-  analogWrite(MotorC2, mid);
-
-  analogWrite(MotorD1, LOW);
-  analogWrite(MotorD2, mid);
-}
+const int resolution = 8;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);   //make sure your Serial Monitor is also set at this baud rate.
 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
   pinMode(SensorLEFT, INPUT);
   pinMode(SensorRIGHT, INPUT);
+
+  pinMode(MotorPWM, OUTPUT);
 
   pinMode(MotorA1, OUTPUT);
   pinMode(MotorA2, OUTPUT);
@@ -124,9 +65,114 @@ void setup() {
 
   pinMode(MotorD1, OUTPUT);
   pinMode(MotorD2, OUTPUT);
+
+  ledcSetup(PWMchannel_1, freq, resolution);
+  ledcAttachPin(MotorA1, PWMchannel_1);
+
+  ledcSetup(PWMchannel_2, freq, resolution);
+  ledcAttachPin(MotorA2, PWMchannel_2);
+
+  ledcSetup(PWMchannel_3, freq, resolution);
+  ledcAttachPin(MotorB1, PWMchannel_3);
+
+  ledcSetup(PWMchannel_4, freq, resolution);
+  ledcAttachPin(MotorB2, PWMchannel_4);
+
+  ledcSetup(PWMchannel_5, freq, resolution);
+  ledcAttachPin(MotorC1, PWMchannel_5);
+
+  ledcSetup(PWMchannel_6, freq, resolution);
+  ledcAttachPin(MotorC2, PWMchannel_6);
+
+  ledcSetup(PWMchannel_7, freq, resolution);
+  ledcAttachPin(MotorD1, PWMchannel_7);
+
+  ledcSetup(PWMchannel_8, freq, resolution);
+  ledcAttachPin(MotorD2, PWMchannel_8);
+}
+
+void forward() {
+  digitalWrite(MotorPWM, HIGH);
+
+  ledcWrite(PWMchannel_1, MID); //MotorA1
+  ledcWrite(PWMchannel_2, LOW); //MotorA2
+
+  ledcWrite(PWMchannel_3, MID); //MotorB1
+  ledcWrite(PWMchannel_4, LOW); //MotorB2
+
+  ledcWrite(PWMchannel_5, MID); //MotorC1
+  ledcWrite(PWMchannel_6, LOW); //MotorC2
+
+  ledcWrite(PWMchannel_7, MID); //MotorD1
+  ledcWrite(PWMchannel_8, LOW); //MotorD2
+}
+
+void right() {
+  digitalWrite(MotorPWM, HIGH);
+
+  ledcWrite(PWMchannel_1, MID);  //MotorA1
+  ledcWrite(PWMchannel_2, LOW);  //MotorA2
+
+  ledcWrite(PWMchannel_3, MID);  //MotorB1
+  ledcWrite(PWMchannel_4, LOW);  //MotorB2
+
+  ledcWrite(PWMchannel_5, LOW);  //MotorC1
+  ledcWrite(PWMchannel_6, MID);  //MotorC2
+
+  ledcWrite(PWMchannel_7, LOW);  //MotorD1
+  ledcWrite(PWMchannel_8, MID);  //MotorD2
+}
+
+void left() {
+  digitalWrite(MotorPWM, HIGH);
+
+  ledcWrite(PWMchannel_1, LOW);  //MotorA1
+  ledcWrite(PWMchannel_2, MID);  //MotorA2
+
+  ledcWrite(PWMchannel_3, LOW);  //MotorB1
+  ledcWrite(PWMchannel_4, MID);  //MotorB2
+
+  ledcWrite(PWMchannel_5, MID);  //MotorC1
+  ledcWrite(PWMchannel_6, LOW);  //MotorC2
+
+  ledcWrite(PWMchannel_7, MID);  //MotorD1
+  ledcWrite(PWMchannel_8, LOW);  //MotorD2
+}
+
+void stop() {
+  digitalWrite(MotorPWM, LOW);
+
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, LOW); //MotorA2
+
+  ledcWrite(PWMchannel_3, LOW); //MotorB1
+  ledcWrite(PWMchannel_4, LOW); //MotorB2
+
+  ledcWrite(PWMchannel_5, LOW); //MotorC1
+  ledcWrite(PWMchannel_6, LOW); //MotorC2
+
+  ledcWrite(PWMchannel_7, LOW); //MotorD1
+  ledcWrite(PWMchannel_8, LOW); //MotorD2
+}
+
+void backward() {
+  digitalWrite(MotorPWM, HIGH);
+  
+  ledcWrite(PWMchannel_1, LOW); //MotorA1
+  ledcWrite(PWMchannel_2, MID); //MotorA2
+
+  ledcWrite(PWMchannel_3, LOW); //MotorB1
+  ledcWrite(PWMchannel_4, MID); //MotorB2
+
+  ledcWrite(PWMchannel_5, LOW); //MotorC1
+  ledcWrite(PWMchannel_6, MID); //MotorC2
+
+  ledcWrite(PWMchannel_7, LOW); //MotorD1
+  ledcWrite(PWMchannel_8, MID); //MotorD2
 }
 
 void loop() {
+  delay(40);
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
@@ -135,33 +181,53 @@ void loop() {
 
   duration = pulseIn(echoPin, HIGH);
   cm = (duration / 2) / 29.1;
-  /* Serial.print(cm);
-  Serial.print("cm");
-  Serial.print("  ");*/
-  delay(100);
-
-
-  int leftSensor = analogRead(SensorLEFT);
-  int rightSensor = analogRead(SensorRIGHT);
-
   /*
-  Serial.print("leftSensor: ");
-  Serial.print(leftSensor);
-  Serial.print("  rightSensor: ");
-  Serial.println(rightSensor);
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println("  ");
   */
-
-  if ((leftSensor < THRESHOLD && rightSensor < THRESHOLD)) {
-    stop();
-    delay(500);
-    backward();
-    delay(800);
-    left();
-    delay(400);
-
-  } else if ((cm < 15) && (leftSensor >= THRESHOLD && rightSensor >= THRESHOLD)) {
-    forward();
-  } else {
-    slowforward();
+  if(cm <= 15){
+    int leftSensor = analogRead(SensorLEFT);
+    int rightSensor = analogRead(SensorRIGHT);
+    /*
+    Serial.print("leftSensor: ");
+    Serial.print(leftSensor);
+    Serial.print("  rightSensor: ");
+    Serial.println(rightSensor);
+    */
+    delay(2);
+    if ((leftSensor >= THRESHOLD) || (rightSensor >= THRESHOLD)) {
+      backward();
+      delay(150);
+    }
+    else if ((leftSensor < THRESHOLD) && (rightSensor < THRESHOLD)) {
+      forward();
+    }
+    else{
+      stop();
+    }
+  }
+  else{
+    int leftSensor = analogRead(SensorLEFT);
+    int rightSensor = analogRead(SensorRIGHT);
+    /*
+    Serial.print("leftSensor: ");
+    Serial.print(leftSensor);
+    Serial.print("  rightSensor: ");
+    Serial.println(rightSensor);
+    */
+    delay(2);
+    if ((leftSensor >= THRESHOLD) || (rightSensor >= THRESHOLD)) {
+      backward();
+      delay(150);
+    }
+    else if ((leftSensor < THRESHOLD) && (rightSensor < THRESHOLD)) {
+      left();
+      delay(50);
+      stop();
+    }
+    else{
+      stop();
+    }
   }
 }
