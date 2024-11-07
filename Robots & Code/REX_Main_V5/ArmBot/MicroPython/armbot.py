df43d75977722cc88148e1bbf23497852e382f8f
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-11-07 10:54:04 +0300

    Update armbot.py

--- Robots & Code/REX_Main_V5/ArmBot/MicroPython/armbot.py
+++ Robots & Code/REX_Main_V5/ArmBot/MicroPython/armbot.py
@@ -62,7 +62,7 @@ Servo_Motor = 1
 MotorSpeed = 65535
 
 def forward(speed):
-   motor_pwm.high()
+   motor_pwm.value(1)
 
    motor_A1.duty_u16(speed)
    motor_A2.duty_u16(0)
@@ -77,7 +77,7 @@ def forward(speed):
    motor_D2.duty_u16(0)
 
 def backward(speed):
-   motor_pwm.high()
+   motor_pwm.value(1)
 
    motor_A1.duty_u16(0)
    motor_A2.duty_u16(speed)
@@ -92,7 +92,7 @@ def backward(speed):
    motor_D2.duty_u16(speed)
 
 def right(speed):
-   motor_pwm.high()
+   motor_pwm.value(1)
    
    motor_A1.duty_u16(speed)
    motor_A2.duty_u16(0)
@@ -107,7 +107,7 @@ def right(speed):
    motor_D2.duty_u16(speed)
 
 def left(speed):
-   motor_pwm.high()
+   motor_pwm.value(1)
    
    motor_A1.duty_u16(0)
    motor_A2.duty_u16(speed)
@@ -122,7 +122,7 @@ def left(speed):
    motor_D2.duty_u16(0)
 
 def stop():
-   motor_pwm.low()
+   motor_pwm.value(0)
    
    motor_A1.duty_u16(0)
    motor_A2.duty_u16(0)
