1feb47b6ed8e20fbbd28b5c2190c569ed0e22451
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-12-12 13:33:50 +0300

    Update BalanceBot.ino

--- Robots & Code/REX_Main_V4/BalanceBot/BalanceBot.ino
+++ Robots & Code/REX_Main_V4/BalanceBot/BalanceBot.ino
@@ -1,5 +1,5 @@
 //"""REX 8in1 Balance Bot"""
-//Check the web site for Robots https://rex-rdt.readthedocs.io/en/latest/
+// ESP32 Board Version Must Be 2.0.11
 
 #include "I2Cdev.h"
 #include "PID_v1.h" 
