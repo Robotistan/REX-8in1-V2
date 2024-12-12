42e5e683cc18d76628744d590c0b4c05a03fb901
AtakanOztrk <134678576+AtakanOztrk@users.noreply.github.com>
2024-12-12 13:29:38 +0300

    Update ArmBot.ino

--- Robots & Code/REX_Main_V5/ArmBot/Arduino/ArmBot.ino
+++ Robots & Code/REX_Main_V5/ArmBot/Arduino/ArmBot.ino
@@ -1,5 +1,5 @@
 //"""REX 8in1 Arm Bot"""
-//Check the web site for Robots https://rex-rdt.readthedocs.io/en/latest/
+// ESP32 Board Version Must Be 2.0.11
 
 #define CUSTOM_SETTINGS
 #define INCLUDE_GAMEPAD_MODULE
